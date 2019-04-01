a2eb66808d64e2e828bc282e0a9d5b4ca46179d7
daniel-kashin <dskashin_1@edu.hse.ru>
2019-04-01 17:39:31 +0300

    Fix requestbody

--- app/src/main/java/com/edoctor/data/remote/api/AccountRestApi.kt
+++ app/src/main/java/com/edoctor/data/remote/api/AccountRestApi.kt
@@ -2,16 +2,19 @@ package com.edoctor.data.remote.api
 
 import com.edoctor.data.entity.remote.response.UserResponseWrapper
 import io.reactivex.Single
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AccountRestApi {
 
     @GET("/account")
-    fun getAccount() : Single<UserResponseWrapper>
+    fun getAccount(): Single<UserResponseWrapper>
 
-//    TODO
+    //    TODO
     @POST("/account")
-    fun updateAccount(userResult: UserResponseWrapper): Single<UserResponseWrapper>
+    fun updateAccount(
+        @Body userRequest: UserResponseWrapper
+    ): Single<UserResponseWrapper>
 
 }
