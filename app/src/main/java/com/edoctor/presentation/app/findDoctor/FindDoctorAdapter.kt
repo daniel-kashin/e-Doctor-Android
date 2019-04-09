17b04a46df7f473011ed342212eda5d6c30a1755
daniel-kashin <dskashin_1@edu.hse.ru>
2019-04-09 19:47:19 +0300

    Find doctor design

--- app/src/main/java/com/edoctor/presentation/app/findDoctor/FindDoctorAdapter.kt
+++ app/src/main/java/com/edoctor/presentation/app/findDoctor/FindDoctorAdapter.kt
@@ -3,8 +3,12 @@ package com.edoctor.presentation.app.findDoctor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.edoctor.R
 import com.edoctor.data.entity.remote.model.user.DoctorModel
 import com.edoctor.presentation.app.findDoctor.FindDoctorAdapter.FindDoctorViewHolder
@@ -40,10 +44,33 @@ class FindDoctorAdapter : RecyclerView.Adapter<FindDoctorViewHolder>() {
         private val onDoctorClickListener: (DoctorModel) -> Unit
     ) : RecyclerView.ViewHolder(rootView) {
 
-        private val doctorEmail by rootView.lazyFind<TextView>(R.id.text_view_doctor_email)
+        private val imageView by rootView.lazyFind<ImageView>(R.id.image_view)
+        private val name by rootView.lazyFind<TextView>(R.id.name)
+        private val specialization by rootView.lazyFind<TextView>(R.id.specialization)
+        private val category by rootView.lazyFind<TextView>(R.id.category)
 
         fun bind(doctor: DoctorModel) {
-            doctorEmail.text = doctor.email
+            name.text = doctor.fullName
+            specialization.text = doctor.specialization
+            category.text = rootView.context.run {
+                when (doctor.category) {
+                    0 -> getString(R.string.doctor_highest_category)
+                    1 -> getString(R.string.doctor_first_category)
+                    2 -> getString(R.string.doctor_second_category)
+                    else -> getString(R.string.doctor_no_category)
+                }
+            }
+            Glide.with(rootView.context)
+                .load(doctor.relativeImageUrl)
+                .apply(
+                    RequestOptions()
+                        .centerCrop()
+                        .placeholder(R.color.lightLightGrey)
+                        .dontAnimate()
+                        .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                )
+                .into(imageView)
+
             rootView.setOnClickListener {
                 onDoctorClickListener(doctor)
             }
